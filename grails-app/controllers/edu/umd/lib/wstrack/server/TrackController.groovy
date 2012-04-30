d54f9f5809eca1c044aafc201c93f95672f67dde
Raghav Manchanda <rmanchan@umd.edu>
2012-04-30 19:30:11 +0000

    Changes added to create or update entry into the Current table.

--- grails-app/controllers/edu/umd/lib/wstrack/server/TrackController.groovy
+++ grails-app/controllers/edu/umd/lib/wstrack/server/TrackController.groovy
@@ -11,27 +11,51 @@ class TrackController {
   def index() {
     render(view:'/error')
   }
-  
+
   def track() {
 
     def result = [status: "success"]
 
     // Input validation
     Boolean guestFlag = (params.guestFlag == 'true')
-    
+
+    /*
+     * @Javadoc - This is the input validation. Only if status is 'login' or 'logout' , the tables 'Current' and 'History' will get populated.
+     * In all other cases, an error message for 'Invalid status' will be rendered.
+     */
     if(params.status == 'login' || params.status == 'Login' || params.status == 'logout' || params.status == 'Logout') {
+
       // Add entry in History
-    def history = new History(guestFlag: guestFlag, hostName: params.hostName,ip : params.ip, os: params.os, status: params.status, userHash : params.userHash)
-    history.save()
+      def history = new History(guestFlag: guestFlag, hostName: params.hostName,ip : params.ip, os: params.os, status: params.status, userHash : params.userHash)
+      history.save()
 
-    // Create or Update entry in Current
-    def current = new Current(guestFlag: guestFlag, hostName: params.hostName,ip : params.ip, os: params.os, status: params.status, userHash : params.userHash)
-    current.timestamp = history.timestamp
-    current.save()
+      // Defining Current Instance which will check if a value exists in the database for a particular IP ( primary Key)
+      def currentInstance = Current.findByIp(params.ip)
 
-    result.current = current
+      if(!currentInstance) {
+        // Create entry in Current
+        def current = new Current(guestFlag: guestFlag, hostName: params.hostName,ip : params.ip, os: params.os, status: params.status, userHash : params.userHash)
+        current.timestamp = history.timestamp
+        current.save()
+        result.current = current
 
-    render result as JSON
+        render result as JSON
+      }
+      else if(currentInstance) {
+        // Update the already existing entry for that particular IP
+        currentInstance.setGuestFlag(guestFlag)
+        currentInstance.setHostName(params.hostName)
+        currentInstance.setOs(params.os)
+        currentInstance.setStatus(params.status)
+        currentInstance.setUserHash(params.userHash)
+        //
+        currentInstance.timestamp = history.timestamp
+        currentInstance.save()
+        result.currentInstance = currentInstance
+        render result as JSON
+      }
+
+
     }
     else {
       params.status = 'error'
