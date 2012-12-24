85c83ee471f140e9dbc92ceb921a0f9607b1facc
Ben Wallberg <wallberg@umd.edu>
2012-12-23 19:58:41 -0500

    populate dev environment with sample data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,12 +1,25 @@
-import edu.umd.lib.wstrack.server.Current;
+import edu.umd.lib.wstrack.server.TrackController
+import grails.util.Environment
 
 class BootStrap {
 
-    def init = { servletContext ->
-        if(!Current.count()) {
-          new Current(ip: "2.2.2.2")
-        }
+  def init = { servletContext ->
+    if (Environment.isDevelopmentMode()) {
+
+      log.info( "Loading sample data..." )
+
+      Map params = [:]
+
+      for (i in 1..1000) {
+        params.computerName = 'computerName' + (i % 10)
+        params.os = 'os' + (i % 3)
+        params.status = ((i % 3 in [1,2]) ? 'login' : 'logout')
+        params .userName = ((i % 15 == 0) ? 'libguest' : '') + 'userName' + (i % 20)
+
+        TrackController.trackX(params)
+      }
     }
-    def destroy = {
-    }
+  }
+  def destroy = {
+  }
 }
--- grails-app/controllers/edu/umd/lib/wstrack/server/TrackController.groovy
+++ grails-app/controllers/edu/umd/lib/wstrack/server/TrackController.groovy
@@ -14,53 +14,52 @@ class TrackController {
 
     def result = [status: "success"]
 
-    // Input validation
-    Boolean guestFlag = (params.userName.startsWith("libguest"))
-    String userHash = generateHash(params.userName)
-
-    /*
-     * @Javadoc - This is the input validation. Only if status is 'login' or 'logout' , the tables 'Current' and 'History' will get populated.
-     * In all other cases, an error message for 'Invalid status' will be rendered.
-     */
     if(params.status == 'login' || params.status == 'Login' || params.status == 'logout' || params.status == 'Logout') {
 
-      // Add entry in History
-      def history = new History(guestFlag: guestFlag, computerName: params.computerName,os: params.os, status: params.status, userHash : userHash)
-      history.save()
+      Current current = trackX(params)
 
-      // Defining Current Instance which will check if a value exists in the database for a particular IP ( primary Key)
-      def current = Current.findByComputerName(params.computerName)
+      result.current = current
 
-      if(!current) {
-        // Create entry in Current
-        current = new Current(guestFlag: guestFlag, computerName: params.computerName, os: params.os, status: params.status, userHash : userHash)
-        current.timestamp = history.timestamp
-        current.save()
-
-        result.current = current
-
-        render result as JSON
-      }
-
-      else if(current) {
-        // Update the already existing entry for that particular IP
-        current.setGuestFlag(guestFlag)
-        current.setOs(params.os)
-        current.setStatus(params.status)
-        current.setUserHash(userHash)
-        current.timestamp = history.timestamp
-        current.save()
-
-        result.current = current
-
-        render result as JSON
-      }
+      render result as JSON
     }
     else {
       params.status = 'error'
       render 'Invalid status. Status should be either login or logout.'
     }
+  }
 
+  static Current trackX(Map params) {
+
+    Boolean guestFlag = (params.userName.startsWith("libguest"))
+    String userHash = generateHash(params.userName)
+
+    // Add entry in History
+    def history = new History(guestFlag: guestFlag, computerName: params.computerName,os: params.os, status: params.status, userHash : userHash)
+    history.save()
+
+    // Defining Current Instance which will check if a value exists in the database for a particular IP ( primary Key)
+    def current = Current.findByComputerName(params.computerName)
+
+    if(!current) {
+      // Create entry in Current
+      current = new Current(guestFlag: guestFlag, computerName: params.computerName, os: params.os, status: params.status, userHash : userHash)
+      current.timestamp = history.timestamp
+      current.save()
+
+      return current
+
+    } else {
+
+      // Update the already existing entry for that particular IP
+      current.setGuestFlag(guestFlag)
+      current.setOs(params.os)
+      current.setStatus(params.status)
+      current.setUserHash(userHash)
+      current.timestamp = history.timestamp
+      current.save()
+
+      return current
+    }
   }
 
   public static String generateHash(String input) {
