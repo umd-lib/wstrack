5385fa4a3204bb74e4837935824f0bb00e45c397
Shrey Hatle <hatlesh@umd.edu>
2014-03-13 14:01:23 -0400

    LIBITD 127. Added the following - Location vs the list of all the systems in that location - Location vs the count of ALL the systems in that location.
    Next Steps - Show availability count.

--- server/grails-app/controllers/edu/umd/lib/wstrack/server/AvailabilityController.groovy
+++ server/grails-app/controllers/edu/umd/lib/wstrack/server/AvailabilityController.groovy
@@ -1,28 +1,137 @@
 package edu.umd.lib.wstrack.server
-import java.util.regex.Pattern
-class AvailabilityController {
-	
-	
+import java.util.regex.*
+import au.com.bytecode.opencsv.CSVReader
+import grails.converters.JSON
 
-    def listAvailable() { 
-		
+/**
+ * 
+ * @author hatlesh
+ *
+ */
+class AvailabilityController {
+
+	static final FILE_PATH = "/apps/git/wstrack/server/location_map.csv"
+	static final PATTERNMAC = '^LIBWK(MCK|NON|ARC|ART|EPL|CHM|MDR|PAL)M[1-7]F(1|3|7)?[0-9]+[a-zA-Z]$'
+	static final PATTERNPC = '^LIBWK(MCK|NON|ARC|ART|EPL|CHM|MDR|PAL)P[1-7]F(1|3|7)?[0-9]+[a-zA-Z]$'
+	static final PATTERN = '^LIBWK(MCK|NON|ARC|ART|EPL|CHM|MDR|PAL)[PM][1-7]F(1|3|7)?[0-9]+[a-zA-Z]$'
+
+	def listAvailable() {
+
 		//Filter out non standard names
 		def matchedAllCurrentList= []
 		matchedAllCurrentList=getAllMatchedCurrentList();
-		
+
 		//Set Locations for the system.
-		setLocations(matchedAllCurrentList)
-		
+		//setLocations(matchedAllCurrentList)
+
 		//This gives the total count of all the systems with standard name.
 		def totalCount = matchedAllCurrentList.size();
-		
+
 		//Filter out all available system available and their count
 		def currentAvailableList = []
 		def currentAvailableCount = 0;
 		currentAvailableList = getAllAvailableSystems(matchedAllCurrentList)
 		currentAvailableCount = currentAvailableList.size()
+
+
+		/******To be put in method that gives
+		 * locationVSLIst of computers in that location*******/
+		def locationMap = getLocationMappingFromCSV();//Mapping of location vs regex
+		 ArrayList<Current> locCurrentList = null//Array list to hold all the systems available in a particular location
+		def locationVsCurrentMap = new HashMap<String,ArrayList<Current>>() //Map that stores the location vs the list of all the systems at that location
 		
-		return [currentAvailableList:currentAvailableList, currentAvailableCount:currentAvailableCount, totalCount:totalCount, matchedAllCurrentList:matchedAllCurrentList]
+		//For location, check all the systems that match
+		for(map in locationMap){
+			locCurrentList = new ArrayList<Current>()
+			//check all the "valid" systems only
+			for(Current tempCurr:matchedAllCurrentList){
+				
+				if(isMatch(tempCurr.getComputerName(),map.value)){
+					//Create an array list of all the systems in a particular location
+					locCurrentList.add(tempCurr)
+				}
+				
+			}
+			if(locCurrentList!=null || locCurrentList.size()!=0){
+				locationVsCurrentMap.put(map.key,locCurrentList)
+			}
+			//Clear the locCurrentList for the next location
+			//locCurrentList.clear()
+		}
+		
+		/*****************************************************/
+		
+		
+		def locationVsCountsMap = new HashMap<String,Map<String,Integer>>() //of the form [Mck 1st floor:{Pc=3,Mac=4}]
+		int macCount=0
+		int pcCount=0
+		String finder=""
+		def countsMap=[]
+		for(map in locationVsCurrentMap){
+			//Reinitialize for the next iteration.
+			macCount=0
+			pcCount=0
+			countsMap = new HashMap<String, Integer>()
+			//iterate over each of the computer and find out the count for pcs and macs
+			for(Current tempCurr:map.value){
+				finder=findComputerOS(tempCurr.getComputerName())
+				println "finder is ${finder}"
+				if(finder.equalsIgnoreCase("PC")){
+					pcCount++
+				}else if(finder.equalsIgnoreCase("MAC")){
+					macCount++
+				}
+				
+			}
+			//Populate the map with the counts
+			countsMap.put("pc",pcCount)
+			countsMap.put("mac",macCount)
+			locationVsCountsMap.put(map.key, countsMap)
+		}
+		
+		/****************************************************/
+
+		def result = [locationVsCurrentMap:locationVsCurrentMap,locationVsCountsMap:locationVsCountsMap]
+
+		//render result as JSON
+		return result
+		//return [locationVsCurrentMap:locationVsCurrentMap,locationVsCountsMap:locationVsCountsMap]
+	}
+
+	/**
+	 * This function returns the OS type MAC/PC
+	 * for a given computer name
+	 * @param localComputerName
+	 * @return
+	 */
+	def findComputerOS(def localComputerName){
+		
+		def match = false
+		
+		match = isMatch(localComputerName,PATTERNMAC)
+		if(match){
+			return "MAC"
+		}else{
+			match = isMatch(localComputerName,PATTERNPC)
+			if(match){
+				return "PC"
+			}
+		}
+	}
+	
+	/**
+	 * This method compares the string to check with the regex pattern
+	 * and returns a true or false based on the match
+	 * @param strToCheck = String to check
+	 * @param regexPattern = Pattern used for string check without the ~/ /
+	 */
+	def isMatch(def strToCheck, def regexPattern){
+		def match = false
+		def pattern = ""
+		pattern = ~/${regexPattern}/
+		assert pattern instanceof Pattern
+		match = pattern.matcher(strToCheck).matches();
+		return match
 	}
 	
 	/**
@@ -33,7 +142,7 @@ class AvailabilityController {
 	void setLocations(def tempList){
 		def loc = ""
 		for(Current curr:tempList){
-			
+
 			loc = getLocation(curr.getComputerName())
 			if(null!=loc || ""!=loc){
 				curr.setLocation(loc)
@@ -42,38 +151,39 @@ class AvailabilityController {
 			}
 		}
 	}
-	
-	
+
+
 	/**
 	 * This class removes all the entries with non-standard computer names
 	 * and then returns the COMPLETE LIST WITH ONLY standard computer names. 
 	 * @return
 	 */
 	List getAllMatchedCurrentList(){
-		
+
 		//This fetches the list with status logout. So basically this
 		//fetches the list of available systems. def currentList = Current.list(fetch: [status:"logout"]);
-		
+
 		//This fetches all the current systems.
 		def currentList = Current.list();
-		def pattern = ~/^LIBWK(MCK|NON|ARC|ART|EPL|CHM|MDR|PAL)[PM][1-7]F(1|3|7)?\d+[a-zA-Z]$/
+		def pattern = ~/${PATTERN}/
+		//$
 		assert  pattern instanceof Pattern
 		boolean match=false;
 		def tempMatchList = []
-		
+
 		for(Current current:currentList){
-			 match = pattern.matcher(current.getComputerName()).matches();
-			 if(match){
-				 tempMatchList.add(current);
-			 }
+			match = pattern.matcher(current.getComputerName()).matches();
+			if(match){
+				tempMatchList.add(current);
+			}
 		}
-		
+
 		return tempMatchList
-		
-		
-	//	
+
+
+		//
 	}
-	
+
 	/**
 	 * This method returns the list of all the systems that are available.
 	 * Since it uses the current list, which only updates the status of a system, 
@@ -82,7 +192,7 @@ class AvailabilityController {
 	 * @return
 	 * @Validateable
 	 */
-	
+
 	def getAllAvailableSystems(def matchedAllCurrentList){
 		def tempCurrentListByStatus = []
 		//tempCurrentListByStatus =  matchedAllCurrentList.list(fetch: [status:"logout"]);
@@ -93,16 +203,16 @@ class AvailabilityController {
 		}
 		return tempCurrentListByStatus
 	}
-	
-	/**
+
+/*	/**
 	 * This method returns a string location based on the
 	 * name of the computer. It matches the computer name with the
 	 * location map and returns a string accordingly.
 	 * @param tempComputerName
 	 * @return
-	 */
-	def getLocation(def tempComputerName){
-		
+	 *
+	def getLocationByComputerName(def tempComputerName){
+
 		println "for computer name ${tempComputerName}"
 		/**
 		 * This is the locationMap that contains the regex for
@@ -111,52 +221,79 @@ class AvailabilityController {
 		 * this off a configuration file (csv,txt etc) and should be done in the
 		 * later releases.
 		 *
-		 */
-		def locationMap = ['^LIBWKMCK[PM]1F[0-9]+[a-zA-Z]$':'McKeldin Library 1st floor',
-			'^LIBWKMCK[PM]2F[0-9]+[a-zA-Z]$':'McKeldin Library 2nd floor',
-			'^LIBWKMCK[PM]4F[0-9]+[a-zA-Z]$':'McKeldin Library 4th floor',
-			'^LIBWKMCK[PM]5F[0-9]+[a-zA-Z]$':'McKeldin Library 5th floor',
-			'^LIBWKMCK[PM]6F[0-9]+[a-zA-Z]$':'McKeldin Library 6th floor',
-			'^LIBWKMCK[PM]6F1[0-9]+[a-zA-Z]$':'McKeldin Library 6th floor RM 6101',
-			'^LIBWKMCK[PM]6F3[0-9]+[a-zA-Z]$':'McKeldin Library 6th floor RM 6103',
-			'^LIBWKMCK[PM]6F7[0-9]+[a-zA-Z]$':'McKeldin Library 6th floor RM 6107',
-			'^LIBWKMCK[PM]7F[0-9]+[a-zA-Z]$':'McKeldin Library 7th floor',
-			'^LIBWKEPL[PM]1F[0-9]+[a-zA-Z]$':'Engineering Library 1st floor',
-			'^LIBWKEPL[PM]2F[0-9]+[a-zA-Z]$':'Engineering Library 2nd floor',
-			'^LIBWKEPL[PM]3F[0-9]+[a-zA-Z]$':'Engineering Library 3rd floor',
-			'^LIBWKCHM[PM]1F[0-9]+[a-zA-Z]$':'Chemistry Library 1st floor',
-			'^LIBWKCHM[PM]2F[0-9]+[a-zA-Z]$':'Chemistry Library 2nd floor',
-			'^LIBWKCHM[PM]3F[0-9]+[a-zA-Z]$':'Chemistry Library 3rd floor',
-			'^LIBWKNON[PM]1F[0-9]+[a-zA-Z]$':'Nonprint Library 1st floor',
-			'^LIBWKMDR[PM]1F[0-9]+[a-zA-Z]$':'MARYLANDIA',
-			'^LIBWKPAL[PM]1F[0-9]+[a-zA-Z]$':'PAL 1st floor',
-			'^LIBWKPAL[PM]2F[0-9]+[a-zA-Z]$':'PAL 2nd floor',
-			'^LIBWKART[PM]1F[0-9]+[a-zA-Z]$':'Art Library 1st floor',
-			'^LIBWKARC[PM]1F[0-9]+[a-zA-Z]$':'Arch Library'
-			]
-		
+		 *
+		def locationMap = getLocationMappingFromCSV();
+
+		//		def locationMap= ['^LIBWKMCK[PM]1F[0-9]+[a-zA-Z]$':'McKeldin Library 1st floor',
+		//		 '^LIBWKMCK[PM]2F[0-9]+[a-zA-Z]$':'McKeldin Library 2nd floor',
+		//		 '^LIBWKMCK[PM]4F[0-9]+[a-zA-Z]$':'McKeldin Library 4th floor',
+		//		 '^LIBWKMCK[PM]5F[0-9]+[a-zA-Z]$':'McKeldin Library 5th floor',
+		//		 '^LIBWKMCK[PM]6F[0-9]+[a-zA-Z]$':'McKeldin Library 6th floor',
+		//		 '^LIBWKMCK[PM]6F1[0-9]+[a-zA-Z]$':'McKeldin Library 6th floor RM 6101',
+		//		 '^LIBWKMCK[PM]6F3[0-9]+[a-zA-Z]$':'McKeldin Library 6th floor RM 6103',
+		//		 '^LIBWKMCK[PM]6F7[0-9]+[a-zA-Z]$':'McKeldin Library 6th floor RM 6107',
+		//		 '^LIBWKMCK[PM]7F[0-9]+[a-zA-Z]$':'McKeldin Library 7th floor',
+		//		 '^LIBWKEPL[PM]1F[0-9]+[a-zA-Z]$':'Engineering Library 1st floor',
+		//		 '^LIBWKEPL[PM]2F[0-9]+[a-zA-Z]$':'Engineering Library 2nd floor',
+		//		 '^LIBWKEPL[PM]3F[0-9]+[a-zA-Z]$':'Engineering Library 3rd floor',
+		//		 '^LIBWKCHM[PM]1F[0-9]+[a-zA-Z]$':'Chemistry Library 1st floor',
+		//		 '^LIBWKCHM[PM]2F[0-9]+[a-zA-Z]$':'Chemistry Library 2nd floor',
+		//		 '^LIBWKCHM[PM]3F[0-9]+[a-zA-Z]$':'Chemistry Library 3rd floor',
+		//		 '^LIBWKNON[PM]1F[0-9]+[a-zA-Z]$':'Nonprint Library 1st floor',
+		//		 '^LIBWKMDR[PM]1F[0-9]+[a-zA-Z]$':'MARYLANDIA',
+		//		 '^LIBWKPAL[PM]1F[0-9]+[a-zA-Z]$':'PAL 1st floor',
+		//		 '^LIBWKPAL[PM]2F[0-9]+[a-zA-Z]$':'PAL 2nd floor',
+		//		 '^LIBWKART[PM]1F[0-9]+[a-zA-Z]$':'Art Library 1st floor',
+		//		 '^LIBWKARC[PM]1F[0-9]+[a-zA-Z]$':'Arch Library'
+		//		 ]
+
 		boolean regexCheck = false
-		/**
-		 * Confirming again that this is a valid name. 
-		 * Also, this makes it easier to initialize the pattern 
-		 * variable.
-		 */
-		def pattern = ~/^LIBWK(MCK|NON|ARC|ART|EPL|CHM|MDR|PAL)[PM][1-7]F(1|3|7)?\d+[a-zA-Z]$/
-		
+		def pattern=""
+
 		def retLocation=""
 
 		for (var in locationMap){
 
-			pattern = ~/${var.key}/
+			pattern = ~/${var.value}/
 			//println "Pattern to be checked is ${pattern}"
 			assert pattern instanceof Pattern
 			regexCheck = pattern.matcher(tempComputerName).matches();
 			println "Regex check = ${regexCheck}"
 			if(regexCheck){
-				retLocation = var.value
+				retLocation = var.key
 				return retLocation
 			}
 		}
-		
+
+	}
+	*/
+	
+	/**
+	 * This method gets the location mapping from the FILE_PATH.
+	 * It returns a map of Location vs its regex pattern
+	 * @return locationMap [key as location: value as regex pattern]
+	 */
+	def getLocationMappingFromCSV(){
+		CSVReader reader = new CSVReader(new FileReader(FILE_PATH));
+		String [] nextLine;
+		int counter = 0
+		def retLocationMap=[:]
+		while ((nextLine = reader.readNext()) != null) {
+			//INcrement the count by 1 for every new line.
+			counter++
+
+			if(counter==1){
+				//Skip this iteration since this is the header
+				continue
+			}else{
+				retLocationMap.put(nextLine[1].toString(),nextLine[0].toString())
+
+			}
+
+			// nextLine[] is an array of values from the line
+			//System.out.println(nextLine[0] +" "+ nextLine[1] );
+		}
+
+		return retLocationMap
 	}
 }
--- server/grails-app/views/availability/listAvailable.gsp
+++ server/grails-app/views/availability/listAvailable.gsp
@@ -1,19 +1,21 @@
 <h1>This is the listAvailable gsp page</h1>
 
-<table border="1">
-<tr><th>Computer Name</th><th>Status</th><th>Location</th></tr>
-<g:each in="${currentAvailableList}" var="current">
-	<tr><td>${current.computerName}</td><td> ${current.status}</td><td> ${current.location}</td></tr>
-</g:each>
-</table>
+<table border=2>
+	<tr>
+		<th>Location</th>
+		<th>Workstations</th>
+	</tr>
+	<g:each in="${locationVsCountsMap}" var="map">
+		<tr>
+			<td>
+				${map.key}
+			</td>
+			<td><g:each in="${map.value}" var="ws">
+					${ws.key}=${ws.value}<br>
+				</g:each></td>
 
-<p><h4>Total Workstations available: ${currentAvailableCount }</h4>
-<p><h4>Total Workstations : ${totalCount }</h4>
+		</tr>
 
-<table border="2">
-<tr><th>Complete list of all the Workstations</th></tr>
-<tr><th>Computer Name</th><th>Status</th><th>Location</th></tr>
-<g:each in="${matchedAllCurrentList}" var="current1">
-	<tr><td>${current1.computerName}</td><td> ${current1.status}</td><td> ${current1.location}</td></tr>
-</g:each>
+
+	</g:each>
 </table>
