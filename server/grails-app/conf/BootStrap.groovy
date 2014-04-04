7c49f22593db4e2e892d5ed6d4500bb9856dd455
Shrey Hatle <hatlesh@umd.edu>
2014-04-04 11:53:19 -0400

    LIBITD 198. Fix Bootstrap data
    - Loads 1000 random names
    - Allocates status, OS and workstation numbers randomly.

--- server/grails-app/conf/BootStrap.groovy
+++ server/grails-app/conf/BootStrap.groovy
@@ -6,16 +6,23 @@ class BootStrap {
   def init = { servletContext ->
     if (Environment.isDevelopmentMode()) {
 
-      log.info( "Loading sample data..." )
+      println ("Loading sample data..." )
 
       Map params = [:]
-
+	  def librarylist = ["MCK", "NON","PAL","EPL","CHM","ARC","ART","MDR"]
+	  def osList = ["P","M"]
+	  def floorList = ["1F","2F","3F","4F","5F","6F","6F3","6F1","6F7"]
+	  def statusList = ["Login","Logout"]
+	  Random r =null;
+	  def compName
       for (i in 0..999) {
-        params.computerName = 'computerName' + (i % 11)
-        params.os = 'os' + (i % 5)
-        params.status = ((i % 2) ? 'login' : 'logout')
+		  compName=''
+		  r=new Random();
+        compName= 'LIBWK' + librarylist[r.nextInt(librarylist.size)]
+        params.os = osList[r.nextInt(osList.size)]
+        params.status = statusList[r.nextInt(statusList.size)]
         params .userName = ((i % 17 == 0) ? 'libguest' : '') + 'userName' + (i % 23)
-
+		params.computerName = compName+params.os+floorList[r.nextInt(floorList.size)]+new Random().nextInt(20)+'a'
         TrackController.trackX(params)
       }
 
