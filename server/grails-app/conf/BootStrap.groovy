8ab4825a9a65f79f35b3b36d0052e038176e4340
Shrey Hatle <hatlesh@umd.edu>
2014-04-29 16:00:45 -0400

    LIBITD 220. load production Current table into local for testing
    - Made chagnes to BootStrap and TrackController.
    - Revert by commenting the method "loadFile()" and uncommenting "loadAndTrack()" in the init() method of BootStrap.groovy
    
    (https://issues.umd.edu/browse/LIBITD-220)

--- server/grails-app/conf/BootStrap.groovy
+++ server/grails-app/conf/BootStrap.groovy
@@ -1,3 +1,4 @@
+import au.com.bytecode.opencsv.CSVReader
 import edu.umd.lib.wstrack.marshallers.JsonCustomMarshaller
 import edu.umd.lib.wstrack.marshallers.XmlCustomMarshaller
 import edu.umd.lib.wstrack.server.TrackController
@@ -18,32 +19,63 @@ class BootStrap {
 	  if (Environment.isDevelopmentMode()) {
 
       println ("Loading sample data..." )
+//	  loadAndTrack()
+	  loadFile()
+	  
+	}
+  }
+  
+  def loadFile(){
+	  Map params = [:]
+	  CSVReader reader = new CSVReader(new FileReader("current-2014-04-29.csv"));
+	  String [] nextLine;
+	  int counter = 0
+	  while ((nextLine = reader.readNext()) != null) {
+		  //INcrement the count by 1 for every new line.
+		  counter++
 
-      Map params = [:]
+		  if(counter==1){
+			  //Skip this iteration since this is the header
+			  continue
+		  }else{
+		  params.os = nextLine[3].toString()
+		  params.status =nextLine[4].toString()
+		  params.computerName = nextLine[1].toString()
+		  params.userHash = nextLine[6].toString()
+		  params.guestFlag = nextLine[2].toString()
+		  params.timeStamp = nextLine[5].toString()
+		  
+		  TrackController.trackX(params)
+
+		  }
+	  }
+  }
+  
+  def loadAndTrack(){
+	  
+	  Map params = [:]
 	  def librarylist = ["MCK", "NON","PAL","EPL","CHM","ARC","ART","MDR"]
 	  def osList = ["P","M"]
 	  def floorList = ["1F","2F","3F","4F","5F","6F","6F3","6F1","6F7"]
 	  def statusList = ["login","logout"]
 	  Random r =null;
 	  def compName
-      for (i in 0..999) {
+	  for (i in 0..999) {
 		  compName=''
 		  r=new Random();
-        compName= 'LIBWK' + librarylist[r.nextInt(librarylist.size)]
-        params.os = osList[r.nextInt(osList.size)]
-        params.status = statusList[r.nextInt(statusList.size)]
-        params .userName = ((i % 17 == 0) ? 'libguest' : '') + 'userName' + (i % 23)
+		compName= 'LIBWK' + librarylist[r.nextInt(librarylist.size)]
+		params.os = osList[r.nextInt(osList.size)]
+		params.status = statusList[r.nextInt(statusList.size)]
+		params .userName = ((i % 17 == 0) ? 'libguest' : '') + 'userName' + (i % 23)
 		params.computerName = compName+params.os+floorList[r.nextInt(floorList.size)]+new Random().nextInt(20)+'a'
-        TrackController.trackX(params)
-      }
-
-      params.userName= 'libguestuserName0'
-      params.status='login'
-      TrackController.trackX(params)
-	  
-	  
-	}
+		TrackController.trackX(params)
+	  }
+	  params.userName= 'libguestuserName0'
+	  params.status='login'
+	  TrackController.trackX(params)
   }
+  
+  
   def destroy = {
   }
   
