4228b40b5e653489454f653ffe1202da01c0db38
Mohamed Mohideen Abdul Rasheed <mohideen@umd.edu>
2014-08-28 16:42:41 -0400

    LIBITD-268. Logout not captured from Mac computers running 10.9
    Modified launcher scripts to set USER env var if it doesn't exist.
    
    https://issues.umd.edu/browse/LIBITD-268

--- client/script/login.sh
+++ client/script/login.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+if [ -z "$USER" ]; then
+  if [ -n "$1" ]; then
+    export USER="$1"
+  fi
+fi
 cd /wstrack
 ./wstrack-client.sh login
 exit 0
--- client/script/logout.sh
+++ client/script/logout.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+if [ -z "$USER" ]; then
+  if [ -n "$1" ]; then
+    export USER="$1"
+  fi
+fi
 cd /wstrack
 ./wstrack-client.sh logout
