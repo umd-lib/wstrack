229daeefbdcbeec5a94103a63537782df74453c4
Mohamed Mohideen Abdul Rasheed <mohideen@umd.edu>
2016-04-06 20:02:26 -0400

    LIBITD-396. Create initial WSTrack server app.
    Created home page and test.
    
    https://issues.umd.edu/browse/LIBITD-396

--- server/app/helpers/application_helper.rb
+++ server/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = 'Workstation Tracking'
+    if page_title.blank?
+      base_title
+    else
+      base_title + ' | ' + page_title
+    end
+  end
 end
