505f47adcf56eb067cd08277b01f59e9dbe10fa6
Mohamed Mohideen Abdul Rasheed <mohideen@umd.edu>
2016-04-11 16:01:23 -0400

    LIBITD-396. Create initial WSTrack server app.
    Added scaffold generated code to get all CRUD views and actions.
    
    https://issues.umd.edu/browse/LIBITD-396

--- server/test/controllers/current_status_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class CurrentStatusControllerTest < ActionController::TestCase
-  test 'should get status' do
-    get :statuses
-    assert_response :success
-    assert_select 'title', 'Current Status | Workstation Tracking'
-  end
-end
