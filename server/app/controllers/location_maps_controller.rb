e2b6d160c41edba7faa9d2688d26e347d7f15843
Mohamed Mohideen Abdul Rasheed <mohideen@umd.edu>
2016-04-12 15:27:32 -0400

    Rubocop fixes.

--- server/app/controllers/location_maps_controller.rb
+++ server/app/controllers/location_maps_controller.rb
@@ -62,6 +62,7 @@ class LocationMapsController < ApplicationController
   end
 
   private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_location_map
       @location_map = LocationMap.find(params[:id])
--- server/test/controllers/location_maps_controller_test.rb
+++ server/test/controllers/location_maps_controller_test.rb
@@ -5,18 +5,18 @@ class LocationMapsControllerTest < ActionController::TestCase
     @location_map = location_maps(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:location_maps)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create location_map" do
+  test 'should create location_map' do
     assert_difference('LocationMap.count') do
       post :create, location_map: { code: @location_map.code, regex: @location_map.regex, value: @location_map.value }
     end
@@ -24,22 +24,23 @@ class LocationMapsControllerTest < ActionController::TestCase
     assert_redirected_to location_map_path(assigns(:location_map))
   end
 
-  test "should show location_map" do
+  test 'should show location_map' do
     get :show, id: @location_map
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get :edit, id: @location_map
     assert_response :success
   end
 
-  test "should update location_map" do
-    patch :update, id: @location_map, location_map: { code: @location_map.code, regex: @location_map.regex, value: @location_map.value }
+  test 'should update location_map' do
+    patch :update, id: @location_map, location_map: { code: @location_map.code, regex: @location_map.regex,
+                                                      value: @location_map.value }
     assert_redirected_to location_map_path(assigns(:location_map))
   end
 
-  test "should destroy location_map" do
+  test 'should destroy location_map' do
     assert_difference('LocationMap.count', -1) do
       delete :destroy, id: @location_map
     end
