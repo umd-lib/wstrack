e26e3dd032cebdb5b1720f07547a8aa8ec6d82e2
Mohamed Mohideen Abdul Rasheed <mohideen@umd.edu>
2016-04-12 13:06:21 -0400

    LIBITD-396. Create initial WSTrack server app.
    Scaffold generated location map.
    
    https://issues.umd.edu/browse/LIBITD-396

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160407122731) do
+ActiveRecord::Schema.define(version: 20160412163330) do
 
   create_table "current_statuses", force: :cascade do |t|
     t.string   "workstation_name"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20160407122731) do
     t.boolean  "guest_flag"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+  end
+
+  create_table "location_maps", force: :cascade do |t|
+    t.string   "code"
+    t.string   "value"
+    t.string   "regex"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
