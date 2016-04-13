b44393b12f2371bd9828fa67873a43ebe707fc5e
Mohamed Mohideen Abdul Rasheed <mohideen@umd.edu>
2016-04-13 09:19:12 -0400

    LIBITD-396. Create initial WSTrack server app.
    Added workstation_type column to CurrentStatus model.
    And code to identify and store workstation type (pc or mac) on save.
    
    https://issues.umd.edu/browse/LIBITD-396

--- server/app/models/current_status.rb
+++ server/app/models/current_status.rb
@@ -2,8 +2,16 @@
 # For each tracking update from clients, either an existing CurrentStatus record that matches the workstation_name
 # will be updated, or a new CurrentStatus record will be created.
 class CurrentStatus < ActiveRecord::Base
+  PATTERN_MAC = /(?i)^LIBRWK(MCK|LMS|ARC|ART|EPL|CHM|MDR|PAL)M[1-7B]F(1|3|7)?.*$/
+  PATTERN_PC = /(?i)^LIBRWK(MCK|LMS|ARC|ART|EPL|CHM|MDR|PAL)P[1-7B]F(1|3|7)?.*$/
+
+  MAC = 'MAC'.freeze
+  PC = 'PC'.freeze
+
   before_save { workstation_name.upcase! }
   before_save { status.downcase! }
+  before_save { set_workstation_type }
+  
   default_scope -> { order(updated_at: :desc) }
   VALID_WORKSTATION_NAME_REGEX = /\ALIBRWK(MCK|LMS|ARC|ART|EPL|CHM|MDR|PAL)[PM][1-7B]F(1|3|7)?.*\z/
   validates :workstation_name, presence: true, format: { with: VALID_WORKSTATION_NAME_REGEX },
@@ -18,5 +26,13 @@ class CurrentStatus < ActiveRecord::Base
     self.os = status_hash[:os]
     self.user_hash = status_hash[:user_hash]
     self.guest_flag = status_hash[:guest_flag]
+  end
+
+  def set_workstation_type
+    if PATTERN_MAC.match(workstation_name)
+      self.workstation_type = MAC
+    elsif PATTERN_PC.match(workstation_name)
+      self.workstation_type = PC
+    end
   end
 end
