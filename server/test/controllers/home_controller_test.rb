3f629778c069a9e4027232f8718eeb363e98698a
Mohamed Abdul Rasheed <mohideen@umd.edu>
2022-03-29 14:33:13 -0400

    LIBITD-396. Additional project setup.
    - Added Dockerfiles for production and ci builds.
    - Updated test setup.
    - Added Jenkinsfile
    
    https://issues.umd.edu/browse/LIBITD-396

--- server/test/controllers/home_controller_test.rb
+++ server/test/controllers/home_controller_test.rb
@@ -1,8 +1,10 @@
 require "test_helper"
 
-class HomeControllerTest < ActionDispatch::IntegrationTest
+class HomeControllerTest < ActionController::TestCase
   test "should get index" do
-    get home_index_url
+    get :index
+
     assert_response :success
+    assert_template :index
   end
 end
--- server/test/test_helper.rb
+++ server/test/test_helper.rb
@@ -1,5 +1,29 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+require 'simplecov-rcov'
+
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  SimpleCov::Formatter::RcovFormatter
+]
+SimpleCov.start
+
+# Improved Minitest output (color and progress bar)
+require 'minitest/reporters'
+
+# add  Minitest::Reporters::SpecReporter.new to first param if you want to see
+# what's running so slow.
+Minitest::Reporters.use!(
+  # Minitest::Reporters::SpecReporter.new,
+  Minitest::Reporters::ProgressReporter.new,
+  ENV,
+  Minitest.backtrace_filter
+)
+
+# Suppress puma start/version output when running tests
+Capybara.server = :puma, { Silent: true } # To clean up your test output
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
