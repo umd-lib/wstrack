4e1ac15a020a4448908dc4336b482ae7292a8e69
Mohamed Abdul Rasheed <mohideen@umd.edu>
2022-03-29 14:33:13 -0400

    LIBITD-396. Additional project setup.
    - Added Dockerfiles for production and ci builds.
    - Updated test setup.
    - Added Jenkinsfile
    
    https://issues.umd.edu/browse/LIBITD-396

--- server/test/application_system_test_case.rb
+++ server/test/application_system_test_case.rb
@@ -3,5 +3,17 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  Capybara.register_driver :chrome_headless do |app|
+    options = ::Selenium::WebDriver::Chrome::Options.new
+
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1400,1400')
+
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  end
+
+  #  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :chrome_headless
 end
