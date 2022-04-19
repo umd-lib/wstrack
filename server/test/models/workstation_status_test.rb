a68a0a45f37e287af53af4c31268984e3e27d1df
David P. Steelman <dsteelma@umd.edu>
2022-04-19 07:04:15 -0400

    LIBITD-2131. Added "URL decoding" of "os" workstation status param
    Added a "before_save" hook to the "WorkstationStatus" model to call the
    "url_decode_os" method, which decodes the URL-encoded "os" parameter
    before saving the model.
    
    Also, in routes.rb, named the route that is called by the wstrack client
    as "wstrack_client".
    
    Added tests.
    
    https://issues.umd.edu/browse/LIBITD-2131

--- server/test/models/workstation_status_test.rb
+++ server/test/models/workstation_status_test.rb
@@ -3,7 +3,24 @@
 require 'test_helper'
 
 class WorkstationStatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '"os" param is URL-decoded on save' do
+    os = 'Mac OS X 10.15.3'
+    encoded_os = CGI.escape(os)
+
+    workstation_status = WorkstationStatus.new(
+      workstation_name: 'LIBRWKSTEMM3F383',
+      os: encoded_os,
+      user_hash: 'y3Fu6SqTdoGUdaERmrF4SA==',
+      status: 'login',
+      guest_flag: 'true'
+    )
+
+    workstation_status.save!
+    assert_equal(os, workstation_status.os)
+
+    # If an "os" is provided with spaces, it is unchanged
+    workstation_status.os = 'Test OS with Spaces'
+    workstation_status.save!
+    assert_equal('Test OS with Spaces', workstation_status.os)
+  end
 end
