779b4dadfb514721e128925099db4d119ac558f1
David P. Steelman <dsteelma@umd.edu>
2022-08-16 10:47:12 -0400

    LIBITD-2186. Modified Grouper group comparison to be case-insensitive
    Modified the "roles_allowed?" method in SessionsController to do
    a case-insensitive comparison to the Grouper Group, as it may be
    returned in different uppercase/lowercase forms.
    
    Added a test case for case insensitivity, and modified the
    "login_with_valid_role" method in test_helper.rb to take an
    optional role argument.
    
    https://issues.umd.edu/browse/LIBITD-2186

--- server/test/integration/sessions_controller_test.rb
+++ server/test/integration/sessions_controller_test.rb
@@ -28,4 +28,20 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_redirected_to login_url
   end
+
+  test 'valid role check should be case insensitve' do
+    valid_roles = %w[
+      WorkstationTracking-Administrator
+      workstationtracking-administrator
+      WORKSTATIONTRACKING-ADMINISTRATOR
+    ]
+
+    valid_roles.each do |role|
+      reset_login
+      login_with_valid_role(role)
+
+      get root_url
+      assert_response :success, "'#{role}' was not recognized as a valid role"
+    end
+  end
 end
